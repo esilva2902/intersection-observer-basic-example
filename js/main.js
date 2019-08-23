a78e7f06759c9daf79331d05c45f89a0314f64b4
Erick Morales Silva <erick.silva@localhost.localdomain>
2019-08-22 21:57:23 -0500

    v0.0.2 - Intersection root element was selected correctly from DOM.

--- js/main.js
+++ js/main.js
@@ -3,15 +3,29 @@ let increasingColor = 'rgba(40, 40, 190, ratio)';
 let decreasingColor = 'rgba(190, 40, 40, ratio)';
 
 window.addEventListener('load', (event) => {
-    let intersectionRoot = document.querySelector('main');
+    let intersectionRoot = document.querySelector('#main');
+    console.log(`intersectionRoot: `, intersectionRoot);
     let elements = document.querySelectorAll("#box");
     let thresholds = buildThresholdList(4);
 
     console.log(`thresholds: `, thresholds);
 
+    /**
+     * rootMargin property:
+     * 
+     * We extend 100px beneath the bottom limit in order to
+     * cross observed element before it appears on viewport.
+     * 
+     * We could have established more than 100px. 
+     * 
+     * Indeed we could have set a negative number. If a negative
+     * number were establish the observed element were visible in
+     * the viewport but our root element (intersection root) would 
+     * not cross the observed element yet.
+     */
     createObserver({
         root: intersectionRoot,
-        rootMargin: '0px 0px 0px 0px',
+        rootMargin: '0px 0px 100px 0px',
         threshold: thresholds
 
     }, elements);
